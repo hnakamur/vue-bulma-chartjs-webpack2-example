03d2868badd96e54021422f49b504d8c883e7cca
Hiroaki Nakamura <hnakamur@gmail.com>
2016-11-18 18:24:23 +0900

    Add vue-bulma-chartjs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <chart :type="'pie'" :data="data" :options="options"></chart>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Chart from 'vue-bulma-chartjs'
 
 export default {
-  name: 'app',
   components: {
-    Hello
+    Chart
+  },
+
+  data () {
+    return {
+      data: {
+        labels: ['Sleeping', 'Designing', 'Coding', 'Cycling'],
+        datasets: [
+          {
+            data: [20, 40, 5, 35],
+            backgroundColor: [
+              '#1fc8db',
+              '#fce473',
+              '#42afe3',
+              '#ed6c63',
+              '#97cd76'
+            ]
+          }
+        ]
+      },
+      options: {
+        segmentShowStroke: false
+      }
+    }
   }
 }
 </script>
